6ea52fceef0a9913c6acf0654230875f9c004ad0
Jessica <jegarceztaylor@gmail.com>
2021-05-09 01:09:06 -0500

    started initializing Angular service - going to test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { User } from './user';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'emailgroupapp';
+
+
+export class AppComponent implements OnInit {
+  // public users: User[] = [];
+  public users: User[];
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(){
+    this.getUsers(); 
+  }
+
+  public getUsers(): void {
+    this.userService.getUsers().subscribe(
+      (response: User[]) => {
+        this.users = response
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
 }
